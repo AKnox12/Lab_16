238d792107637f352213a2bdf6e17e42fac7052c
AKnox12 <aknox12@student.cscc.edu>
2025-05-02 16:41:44 -0400

    Added another file, deleted a file, added required comments to the added file.

---
+++ Lab_16_knox_1.py
@@ -0,0 +1,8 @@
+# Python
+# Autumn Knox
+# FRED Economic Data site, for national unemployment rate since 1976
+# May 2nd, 2025
+
+from pathlib import Path
+import csv
+import matplotlib.pyplot as plt
